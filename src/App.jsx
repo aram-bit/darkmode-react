2ecba486c7ae6a24231c4a5a751e718b1eaaf2d3
aramix <aram1988slm@gmail.com>
2024-11-14 16:13:24 +0330

    components and css is created

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./index.css";
+import Form from "./components/Form";
+export default function MyApp() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <div>
+      <Form theme={theme} />
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === "dark"}
+          onChange={(e) => {
+            setTheme(e.target.checked ? "dark" : "light");
+          }}
+        />
+        Use dark mode
+      </label>
+    </div>
+  );
+}
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button({ children, theme }) {
+    const className = "button-" + theme;
+    return <button className={className}>{children}</button>;
+  }
+  
